648993e80ca9b5ed36fd6cba08399eb7b8b4ba2a
Britnay Gore <britnaygore268@gmail.com>
2024-07-06 13:48:58 -0400

    A product can be added

--- Reducto-Absurdum/Product.cs
+++ Reducto-Absurdum/Product.cs
@@ -3,9 +3,8 @@ namespace Reducto_Absurdum;
 
 public class Product
 {
-	public string Name { get; set; }
-	public decimal Price { get; set; }
-	public bool Available { get; set; }
-	public int ProductTypeId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool Available { get; set; }
+    public int ProductTypeId { get; set; }
 }
-
